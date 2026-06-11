6da5bdf6b1ff62fc2a92a25e6cdc0d22ffab2234
Roger Parish III <Roger.Parish3@proton.me>
2026-06-11 16:07:51 -0400

    docs(update.sh): add comments to denote custom llama build commands

--- src/local_llm_scaffolding/llama/build/update.sh
+++ src/local_llm_scaffolding/llama/build/update.sh
@@ -2,8 +2,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR
 git clone https://github.com/ggml-org/llama.cpp
 cd llama.cpp/
+# Replace from here-
 HIPCXX="$(hipconfig -l)/clang" HIP_PATH="$(hipconfig -R)" \
   cmake -S . -B build -DGGML_HIP=ON -DGPU_TARGETS=gfx1030 -DCMAKE_BUILD_TYPE=Release
+# -to here with your custom build settings.
 cmake --build build --target llama-server --config Release -j $(nproc)
 cd ..
 mv llama.cpp/build/bin/* bin/
