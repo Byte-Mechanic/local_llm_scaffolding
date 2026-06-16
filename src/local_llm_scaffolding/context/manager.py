753b3cf864db683bedcb8964668946056f19a674
Roger Parish III <Roger.Parish3@proton.me>
2026-06-15 20:54:03 -0400

    feat(ContextManager): compaction algorithm
    With the vram limitation we have, context will be a huge limiting
    factor, and this was built to help alleviate that. it is not designed to
    be infinite.

--- src/local_llm_scaffolding/context/manager.py
+++ src/local_llm_scaffolding/context/manager.py
@@ -14,6 +14,7 @@ logger.info(f'Logger {logger.name} Initiated.')
 proj_root = pathlib.Path(__file__).resolve().parent.parent
 sys_prompt_file = 'config/agent/system_prompt.md' # get from config after config
 sys_memory_file = 'config/agent/memory.md'        # refactor
+compaction_file = 'context/general_compaction.md'
 user = 'user'
 
 
@@ -30,8 +31,9 @@ class ContextManager:
         self.ctx_token_limit: int = 0
         self.ctx_tokens_used: int = 0
         self.retain_reasoning: bool = True
-        self.tool_instructions = []
-        self.system_prompt = {'role':'system','content':''}
+        self.compact_chunks: dict = {}
+        self.tool_instructions: list = []
+        self.system_prompt: dict = {'role':'system','content':''}
     def build_sys_prompt(self) -> None:
         # Load external files
         with open(proj_root.joinpath(sys_prompt_file), 'r') as doc:
@@ -56,6 +58,9 @@ class ContextManager:
                 msg['content'] = self.system_prompt
         else:
             self.full_context.insert(0, self.system_prompt)
+        self.working_context = copy.deepcopy(self.full_context)
+        logger.info(f'System Prompt built:\n'
+                    f'{pprint.pformat(self.system_prompt)}')
 
     def add_user_msg(self, msg) -> None:
         message: dict = {'role': 'user',
@@ -63,6 +68,7 @@ class ContextManager:
         logger.info(f'Adding User Message:\n'
                     f'{json.dumps(message, indent=4)}')
         self.full_context.append(message)
+        self.working_context.append(message)
         self.post_add()
     def add_assistant_msg(self, msg: GenerationResult) -> None:
         message: dict = {'content': msg['response'],
@@ -74,6 +80,7 @@ class ContextManager:
         logger.info(f'Adding Assistant Message:\n'
                     f'{json.dumps(message, indent=4)}')
         self.full_context.append(message)
+        self.working_context.append(message)
         self.post_add()
     def add_tool_msg(self, tool, tool_result):
         message: dict = {'role': 'tool',
@@ -84,17 +91,89 @@ class ContextManager:
         logger.info(f'Adding Tool Message:\n'
                     f'{json.dumps(message, indent=4)}')
         self.full_context.append(message)
+        self.working_context.append(message)
         self.post_add()
     def post_add(self):
         ## update ctx_tokens_used + ctx_tokens_limit, kick off compaction if 
         ## needed, and update the working_context
-        self.working_context = copy.deepcopy(self.full_context)
         logger.info(f'working_context Updated:\n'
                     f'{json.dumps(self.working_context, indent=4)}')
-        if self.llama_interface.cur_model != '':
+        if self.llama_interface.default_model != '':
             self.ctx_tokens_used = self.llama_interface.count_tokens(self.working_context)
-    def compact_context(self):
-        pass
+    def compact_context(self) -> None:
+        logger.info(f'Starting Context Compaction...')
+        llama = self.llama_interface
+        first_chunk = False
+        # Register compaction chunk in self.compact_chunks
+        ctx_range: tuple = (self.full_context.index(self.working_context[1]),
+                            self.full_context.index(self.working_context[-2]))
+        if self.compact_chunks:
+            comp_keys = list(self.compact_chunks.keys())
+            comp_keys.sort()
+            chunk = comp_keys[-1]+1
+            self.compact_chunks[chunk] = ctx_range
+        else:
+            first_chunk = True
+            chunk = 0
+            self.compact_chunks[chunk] = ctx_range
+        # Generate stats
+        before_tokens = llama.count_tokens(self.working_context)
+        # strip data not to be compacted
+        if self.working_context[0]['role'] == 'system':
+            sys_prompt = self.working_context.pop(0)
+            logger.info(f'System prompt preserved:\n{json.dumps(sys_prompt, 
+                        indent=4)}')
+        else:
+            sys_prompt = {'role':'system',
+                          'content':'You are a helpful AI assistant.'}
+            logger.info(f'No system prompt detected. Using default:\n'
+                        f'{json.dumps(sys_prompt, indent=4)}')
+        if len(self.working_context) > 2:
+            last_msgs: list[dict] = []
+            last_msgs.append(self.working_context.pop(-2))
+            last_msgs.append(self.working_context.pop(-1))
+            logger.info(f'Last messages preserved:\n{json.dumps(last_msgs, 
+                        indent=4)}')
+        else:
+            logger.info('Context cannot be compacted.')
+            # revert
+            self.working_context = copy.deepcopy(self.full_context)
+            return
+        # COMPACTION
+        with open(proj_root.joinpath(compaction_file), 'r') as doc:
+            compact_prompt = [{'role':'system',
+                              'content':doc.read()},
+                              {'role':'user',
+                               'content':json.dumps(self.working_context, 
+                                                   indent=4)}]
+        response = llama.chat_completions(model = 'mini_thinking',
+                                         context = compact_prompt,
+                                         tools = None,
+                                         caching = False)
+        logger.info(f'Compaction:\n{json.dumps(response, indent=4)}')
+        # add to system prompt
+        if first_chunk == True:
+            compact_msg = (f'\n-----------------------------------------------\n'
+                           f'The conversation has been compacted and the\n'
+                           f'context you need to continue is provided below:\n\n'
+                           f'Chunk 0:\n'
+                           f'{response['response']}\n')
+        else:
+            compact_msg = (f'Chunk {chunk}:\n'
+                           f'{response['response']}\n')
+        sys_prompt['content'] += compact_msg
+        # rebuild the context
+        new_context = [sys_prompt]
+        if len(last_msgs) == 2:
+            new_context.extend([last_msgs[-2],last_msgs[-1]])
+        logger.info(f'Post-Compaction Context:\n{json.dumps(new_context, 
+                    indent=4)}')
+        # Apply to the working context
+        self.working_context = copy.deepcopy(new_context)
+        # Log stats
+        after_tokens = llama.count_tokens(new_context)
+        logger.info(f'Compaction Complete [{before_tokens}->{after_tokens}]')
+
     def compact_tool_use(self):
         pass
 
