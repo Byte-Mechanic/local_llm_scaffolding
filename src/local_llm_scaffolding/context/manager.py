60104a4fe584dc0fc30ce0f80b05dbdc4994677b
Roger Parish III <Roger.Parish3@proton.me>
2026-06-16 14:31:49 -0400

    style(ContextManager): swaps json.dumps() for pprint.pformat() in logger inputs
    json.dumps() works for visualization, but in cases where single long
    lines are output in the context, it can be hard to navigate the logs.
    pprint.pformat() limits line lengths to 80 chars (adjusted to 120) and
    continues on a new line, making reading logs easier, plus it formats
    escape characters.

--- src/local_llm_scaffolding/context/manager.py
+++ src/local_llm_scaffolding/context/manager.py
@@ -4,6 +4,7 @@
 import logging
 import json
 import copy
+import pprint
 import datetime
 import pathlib
 from ..llama.interface import LlamaInterface, GenerationResult
@@ -16,7 +17,7 @@ sys_prompt_file = 'config/agent/system_prompt.md' # get from config after config
 sys_memory_file = 'config/agent/memory.md'        # refactor
 compaction_file = 'context/general_compaction.md'
 user = 'user'
-
+log_wd = 120
 
 class ContextManager:
     def __init__(self, llama_interface: LlamaInterface, context: list|None = None):
@@ -27,13 +28,15 @@ class ContextManager:
                                                ### Here
 
         self.llama_interface: LlamaInterface = llama_interface
-        self.working_context: list = []
+        self.working_context: list[dict] = copy.deepcopy(self.full_context)
         self.ctx_token_limit: int = 0
         self.ctx_tokens_used: int = 0
         self.retain_reasoning: bool = True
         self.compact_chunks: dict = {}
         self.tool_instructions: list = []
         self.system_prompt: dict = {'role':'system','content':''}
+        logger.info(f'Logger initialized with context:\n'
+                    f'{pprint.pformat(self.working_context, width=log_wd)}')
     def build_sys_prompt(self) -> None:
         # Load external files
         with open(proj_root.joinpath(sys_prompt_file), 'r') as doc:
@@ -60,13 +63,13 @@ class ContextManager:
             self.full_context.insert(0, self.system_prompt)
         self.working_context = copy.deepcopy(self.full_context)
         logger.info(f'System Prompt built:\n'
-                    f'{pprint.pformat(self.system_prompt)}')
+                    f'{pprint.pformat(self.system_prompt, width=log_wd)}')
 
     def add_user_msg(self, msg) -> None:
         message: dict = {'role': 'user',
                    'content': msg}
         logger.info(f'Adding User Message:\n'
-                    f'{json.dumps(message, indent=4)}')
+                    f'{pprint.pformat(message, width=log_wd)}')
         self.full_context.append(message)
         self.working_context.append(message)
         self.post_add()
@@ -78,7 +81,7 @@ class ContextManager:
         if self.retain_reasoning == True:
             message['reasoning_content'] = msg['reasoning']
         logger.info(f'Adding Assistant Message:\n'
-                    f'{json.dumps(message, indent=4)}')
+                    f'{pprint.pformat(message, width=log_wd)}')
         self.full_context.append(message)
         self.working_context.append(message)
         self.post_add()
@@ -89,7 +92,7 @@ class ContextManager:
                                      if not isinstance(tool_result, str)
                                      else tool_result)}
         logger.info(f'Adding Tool Message:\n'
-                    f'{json.dumps(message, indent=4)}')
+                    f'{pprint.pformat(message, width=log_wd)}')
         self.full_context.append(message)
         self.working_context.append(message)
         self.post_add()
@@ -97,7 +100,7 @@ class ContextManager:
         ## update ctx_tokens_used + ctx_tokens_limit, kick off compaction if 
         ## needed, and update the working_context
         logger.info(f'working_context Updated:\n'
-                    f'{json.dumps(self.working_context, indent=4)}')
+                    f'{pprint.pformat(self.working_context, width=log_wd)}')
         if self.llama_interface.default_model != '':
             self.ctx_tokens_used = self.llama_interface.count_tokens(self.working_context)
     def compact_context(self) -> None:
@@ -121,19 +124,19 @@ class ContextManager:
         # strip data not to be compacted
         if self.working_context[0]['role'] == 'system':
             sys_prompt = self.working_context.pop(0)
-            logger.info(f'System prompt preserved:\n{json.dumps(sys_prompt, 
-                        indent=4)}')
+            logger.info(f'System prompt preserved:\n'
+                        f'{pprint.pformat(sys_prompt, width=log_wd)}')
         else:
             sys_prompt = {'role':'system',
                           'content':'You are a helpful AI assistant.'}
             logger.info(f'No system prompt detected. Using default:\n'
-                        f'{json.dumps(sys_prompt, indent=4)}')
+                        f'{pprint.pformat(sys_prompt, width=log_wd)}')
         if len(self.working_context) > 2:
             last_msgs: list[dict] = []
             last_msgs.append(self.working_context.pop(-2))
             last_msgs.append(self.working_context.pop(-1))
-            logger.info(f'Last messages preserved:\n{json.dumps(last_msgs, 
-                        indent=4)}')
+            logger.info(f'Last messages preserved:\n'
+                        f'{pprint.pformat(last_msgs, width=log_wd)}')
         else:
             logger.info('Context cannot be compacted.')
             # revert
@@ -150,7 +153,7 @@ class ContextManager:
                                          context = compact_prompt,
                                          tools = None,
                                          caching = False)
-        logger.info(f'Compaction:\n{json.dumps(response, indent=4)}')
+        logger.info(f'Compaction:\n{pprint.pformat(response, width=log_wd)}')
         # add to system prompt
         if first_chunk == True:
             compact_msg = (f'\n-----------------------------------------------\n'
@@ -166,8 +169,8 @@ class ContextManager:
         new_context = [sys_prompt]
         if len(last_msgs) == 2:
             new_context.extend([last_msgs[-2],last_msgs[-1]])
-        logger.info(f'Post-Compaction Context:\n{json.dumps(new_context, 
-                    indent=4)}')
+        logger.info(f'Post-Compaction Context:\n'
+                    f'{pprint.pformat(new_context, width=log_wd)}')
         # Apply to the working context
         self.working_context = copy.deepcopy(new_context)
         # Log stats
