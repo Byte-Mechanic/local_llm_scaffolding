59c858dc10c34dfde61d3cb9e7c89f91a6e0fabf
Roger Parish III <Roger.Parish3@proton.me>
2026-06-29 10:30:35 -0400

    feat(Tools): error handling for input data
    This strictly enforces the relevant data shape needed for tool calling,
    and fails before attempting to call any tools. This protects against
    potential damage due to malformed tool calls that pass through anyways.
    This also raises specific errors for the caller to act upon.

--- src/local_llm_scaffolding/tools/handler.py
+++ src/local_llm_scaffolding/tools/handler.py
@@ -26,16 +26,13 @@ logger.info(f'Logger "{logger.name}" Initiated.')
 
 class Tools:
     def __init__(self, llama_interface, context_manager):
-        
         # Interfaces
         self.interfaces = {'llama':llama_interface,
                            'context':context_manager}
-        
         # Init vars for tools loaded below.
         self.tool_definitions: list = []
         self.funct_map: dict = {}
         self.system_prompt_injections: list = []
-
         self.ignore_files: list = ['__pycache__', '__init__.py', 'handler.py', 
                                    'utils.py', 'tool_log.log']
         for tool_dir in (pathlib.Path(__file__).parent.resolve().iterdir()): #Add to config
@@ -68,6 +65,7 @@ class Tools:
                     del self.funct_map[tool_name]
                 del tool_module
         self.send_instructions_to_context_mgr()
+
     def _load_tool_modules(self, int_name: str, path: pathlib.Path):
         module = importlib.import_module(f".{int_name}.tool", package=__package__)
         return module
@@ -75,36 +73,48 @@ class Tools:
         if len(self.system_prompt_injections) >= 1:
             self.interfaces['context'].tool_instructions = (
                     self.system_prompt_injections)
+
         self.interfaces['context'].build_sys_prompt()
     def update_tool_schema(self, path: pathlib.Path) -> dict:
         with open(path.joinpath('definition.json'), 'r') as doc:
             return json.loads(doc.read())
-    def execute_tool(self, tool: dict):
-        ## Add type checking for input validation
-        if isinstance(tool, dict):
-            tool_result = {}
-            
-            funct_name = tool['function']['name']
-            try:
-                funct_args = json.loads(tool['function']['arguments'])
-            except json.decoder.JSONDecodeError as e:
-                logger.error(f'Arguments could not be parsed by the JSON '
-                             f'decoder.\nJSON: {tool['function']['arguments']}'
-                             f'\nError: {e}')
-                funct_args = (f'Arguments could not be parsed by'
-                              f' JSON decoder. \nError: {e}')
-            raw_tool_result = self.funct_map[funct_name](self.interfaces, 
-                                                         **funct_args)
-            tool_result = {'funct_name': funct_name,
-                           'funct_args': funct_args,
-                           'result': raw_tool_result}
 
-            logger.info(f'Returning Tool Result:\n{json.dumps(tool_result, 
-                        indent=4)}')
-            return tool_result
-        else:
+    def execute_tool(self, tool: dict):
+        ## Validate input
+        tool_shape = {'type', 'function','id'}
+        funct_shape = {'name', 'arguments'}
+        if not isinstance(tool, dict):
             logger.critical(f'Tool call is not of ecpected type "dict":\n{
                             json.dumps(tool, indent=4)}')
             raise ValueError
-
+        if not tool_shape.issubset(tool.keys()):
+            logger.critical(f'Tool schema not matching expected input.\n'
+                            f'{json.dumps(tool, indent=4)}')
+            raise KeyError
+        if not funct_shape.issubset(tool['function'].keys()):
+            logger.critical(f'Tool schema not matching expected input.\n'
+                            f'{json.dumps(tool, indent=4)}')
+            raise KeyError
+        tool_result = {}
+        funct_name: str = tool['function']['name']
+        try:
+            funct_args = json.loads(tool['function']['arguments'])
+        except json.decoder.JSONDecodeError as e:
+            raw_tool_result = (f'Your arguments could not be parsed by the '
+                               f'JSON decoder.\nYour previous tool call: '
+                               f'{tool}\nThe error: {e}\nCorrect your call and'
+                               f' try again.')
+            logger.error(f'Arguments could not be parsed by'
+                         f' JSON decoder. \nError: {e}')
+            return {'funct_name': 'ERROR',
+                    'funct_args': 'ERROR',
+                    'result': raw_tool_result}
+        raw_tool_result = self.funct_map[funct_name](self.interfaces, 
+                                                     **funct_args)
+        tool_result = {'funct_name': funct_name,
+                       'funct_args': funct_args,
+                       'result': raw_tool_result}
+        logger.info(f'Returning Tool Result:\n{json.dumps(tool_result, 
+                    indent=4)}')
+        return tool_result
 
